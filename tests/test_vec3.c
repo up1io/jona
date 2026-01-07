63559ba4d63768fb0c46bd921f89b1d0ff15a8b0
Jonathan Mausbach <mausbach@up1.io>
2026-01-07 21:27:12 +0100

    task: added unit tests for basic v3 arithemtic operations

--- tests/test_vec3.c
+++ tests/test_vec3.c
@@ -175,6 +175,174 @@ void test_vec3_mul_mix(void)
   jo_test_print_ok(&test);
 }
 
+void test_vec3_scale(void)
+{
+  vec3_t v = vec3(2.0f, 2.0f, 2.0f);
+  float32_t s = 2.0f;
+
+  test_t test;
+  jo_test_init(&test, "test_vec3_scale");
+
+  vec3_t result = vec3_scale(v, s);
+
+  jo_test_time_stop(&test);
+
+  assert(result.x == 4.0f);
+  assert(result.y == 4.0f);
+  assert(result.z == 4.0f);
+
+  jo_test_print_ok(&test);
+}
+
+void test_vec3_scale_negative()
+{
+  vec3_t v = vec3(2.0f, 2.0f, 2.0f);
+  float32_t s = 0.5f;
+
+  test_t test;
+  jo_test_init(&test, "test_vec3_scale_negativ");
+
+  vec3_t result = vec3_scale(v, s);
+
+  jo_test_time_stop(&test);
+
+  assert(result.x == 1.0f);
+  assert(result.y == 1.0f);
+  assert(result.z == 1.0f);
+
+  jo_test_print_ok(&test);
+}
+
+void test_vec3_scale_mix()
+{
+  vec3_t v = vec3(4.0f, 10.0f, 8.0f);
+  float32_t s = 2.0f;
+
+  test_t test;
+  jo_test_init(&test, "test_vec3_scale_mix");
+
+  vec3_t result = vec3_scale(v, s);
+  
+  jo_test_time_stop(&test);
+
+  assert(result.x == 8.0f);
+  assert(result.y == 20.0f);
+  assert(result.z == 16.0f);
+
+  jo_test_print_ok(&test);
+}
+
+void test_vec3_div(void)
+{
+  vec3_t v = vec3(4.0f, 4.0f, 4.0f);
+  float32_t s = 2.0f;
+
+  test_t test;
+  jo_test_init(&test, "test_vec3_div");
+
+  vec3_t result = vec3_div(v, s);
+
+  jo_test_time_stop(&test);
+
+  assert(result.x == 2.0f);
+  assert(result.y == 2.0f);
+  assert(result.z == 2.0f);
+
+  jo_test_print_ok(&test);
+}
+
+void test_vec3_div_negative(void)
+{
+  vec3_t v = vec3(-4.0f, -4.0f, -4.0f);
+  float32_t s = 2.0f;
+
+  test_t test;
+  jo_test_init(&test, "test_vec3_div_negative");
+
+  vec3_t result = vec3_div(v, s);
+
+  jo_test_time_stop(&test);
+
+  assert(result.x == -2.0f);
+  assert(result.y == -2.0f);
+  assert(result.z == -2.0f);
+
+  jo_test_print_ok(&test);
+}
+
+void test_vec3_div_mix(void)
+{
+  vec3_t v = vec3(10.0f, -4.0f, 7.5f);
+  float32_t s = 2.0f;
+
+  test_t test;
+  jo_test_init(&test, "test_vec3_div_mix");
+  
+  vec3_t result = vec3_div(v, s);
+
+  jo_test_time_stop(&test);
+
+  assert(result.x == 5.0f);
+  assert(result.y == -2.0f);
+  assert(result.z == 3.75f);
+
+  jo_test_print_ok(&test);
+}
+
+void test_vec3_negate(void)
+{
+  vec3_t v = vec3(2.0f, 2.0f, 2.0f);
+
+  test_t test;
+  jo_test_init(&test, "test_vec3_negate");
+
+  vec3_t result = vec3_negate(v);
+
+  jo_test_time_stop(&test);
+
+  assert(result.x == -2.0f);
+  assert(result.y == -2.0f);
+  assert(result.z == -2.0f);
+
+  jo_test_print_ok(&test);
+}
+
+void test_vec3_negate_negative(void)
+{
+  vec3_t v = vec3(-2.0f, -2.0f, -2.0f);
+
+  test_t test;
+  jo_test_init(&test, "test_vec3_negate_negative");
+
+  vec3_t result = vec3_negate(v);
+
+  jo_test_time_stop(&test);
+
+  assert(result.x == 2.0f);
+  assert(result.y == 2.0f);
+  assert(result.z == 2.0f);
+
+  jo_test_print_ok(&test);
+}
+
+void test_vec3_negate_mix(void)
+{
+  vec3_t v = vec3(10.0f, -4.0f, 3.5f);
+
+  test_t test;
+  jo_test_init(&test, "test_vec3_negate_mix");
+
+  vec3_t result = vec3_negate(v);
+
+  jo_test_time_stop(&test);
+
+  assert(result.x == -10.0f);
+  assert(result.y == 4.0f);
+  assert(result.z == -3.5f);
+
+  jo_test_print_ok(&test);
+}
+
 int main(void)
 {
   printf("\nRunning vec3 tests\n\n");
@@ -194,6 +362,24 @@ int main(void)
   test_vec3_mul();
   test_vec3_mul_negative();
   test_vec3_mul_mix();
+
+  printf("\n");
+
+  test_vec3_scale();
+  test_vec3_scale_negative();
+  test_vec3_scale_mix();
+
+  printf("\n");
+
+  test_vec3_div();
+  test_vec3_div_negative();
+  test_vec3_div_mix();
+
+  printf("\n");
+
+  test_vec3_negate();
+  test_vec3_negate_negative();
+  test_vec3_negate_mix();
 
   printf("\nAll tests passed!\n");
   return 0;
